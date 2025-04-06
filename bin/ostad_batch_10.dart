0198463b0ae64d5d69d026c1e5345bba3f5c004c
Mahid <mdmahid559@gmail.com>
2025-04-07 01:11:21 +0800

    live test 1

--- bin/ostad_batch_10.dart
+++ bin/ostad_batch_10.dart
@@ -1,13 +1,24 @@
-abstract class Vehicle {
-  int _speed = 0;
-
-  void move();
-
+import 'dart:collection';
 
 void main() {
-  Car myCar = Car();
+  List<Map<String, dynamic>> students = [
+    {"name": "Alice", "scores": [85, 90, 78]},
+    {"name": "Bob", "scores": [88, 76, 95]},
+    {"name": "Charlie", "scores": [90, 92, 85]}
+  ];
 
-  myCar.setSpeed(80);
+  Map<String, double> averageScores = {};
+  for (var student in students) {
+    String name = student["name"];
+    List<int> scores = List<int>.from(student["scores"]);
+    double average = scores.reduce((a, b) => a + b) / scores.length;
+    averageScores[name] = double.parse(average.toStringAsFixed(2));
+  }
 
-  myCar.move();
+  var sortedEntries = averageScores.entries.toList()
+    ..sort((a, b) => b.value.compareTo(a.value));
+
+  LinkedHashMap<String, double> sortedMap = LinkedHashMap.fromEntries(sortedEntries);
+
+  print(sortedMap);
 }
