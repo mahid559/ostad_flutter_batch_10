1c8c212f0a2a0d28f48bab53f4266aa6a1cc6b47
Mahid <mdmahid559@gmail.com>
2025-04-06 23:58:46 +0800

    update

---
+++ bin/ostad_batch_10.dart
@@ -0,0 +1,5 @@
+import 'package:ostad_batch_10/ostad_batch_10.dart' as ostad_batch_10;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ostad_batch_10.calculate()}!');
+}
---
+++ test/ostad_batch_10_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_batch_10/ostad_batch_10.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
